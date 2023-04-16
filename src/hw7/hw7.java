045a21e523d9c063d6d5414f36a055d04540fa93
B-Jev <baldenmax@gmail.com>
2023-04-16 19:20:41 +0300

    Обьекты и классы.

---
+++ src/hw7/hw7.java
@@ -0,0 +1,24 @@
+package hw7;
+
+public class hw7 {
+
+
+    public static void main(String[] args) {
+        Author tolstoi = new Author("Лев", "Толстой");
+        Book warAndPeace = new Book("Воина и мир", 1867);
+
+        Author gamilton = new Author("Эдмонд", "Гамильтон");
+        Book starKings = new Book("Звёздные короли", 1947);
+
+        System.out.println("Автор книги - " + tolstoi.getName() + " " + tolstoi.getSurname());
+        System.out.println("Название книги - " + warAndPeace.getNameBook());
+        System.out.println("Дата публикации - " + warAndPeace.getYearBook());
+
+        System.out.println("Автор книги - " + gamilton.getName() + " " + gamilton.getSurname());
+        System.out.println("Название книги - " + starKings.getNameBook());
+        System.out.println("Дата публикации - " + starKings.getYearBook());
+
+        starKings.setYearBook(1949);
+        System.out.println("Дата публикации - " + starKings.getYearBook());
+    }
+}
